d16fd1b345281b1770e1ea77c2266c6815b4bb6f
PROTOTYPE9111 <prototype9111@list.ru>
2025-12-02 02:38:13 +0300

    Added all folders and ex 1-2

---
+++ 1_easy_examples/example_2.cpp
@@ -0,0 +1,14 @@
+// Число, введенное пользователем
+
+#include <iostream>
+using namespace std; 
+
+int main(){
+    int number; 
+
+    cout << "Введите число: ";
+    cin >> number;
+
+    cout << "Вы ввели " << number << "\n";
+    return 0;
+}
