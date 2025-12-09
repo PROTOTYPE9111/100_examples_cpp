206430cada33b063a9239a4ae442804326c9ad7d
PROTOTYPE9111 <prototype9111@list.ru>
2025-12-10 00:44:15 +0300

    new

---
+++ 1_easy_examples/example_8.cpp
@@ -0,0 +1,14 @@
+// Умножение 2х вещественных чисел
+
+#include <iostream>
+using namespace std; 
+
+int main(){
+    double f, s, p;
+    cout << "введите 2 числа: ";
+    cin >> f >> s;
+
+    p = f * s;
+
+    cout << p;
+}
